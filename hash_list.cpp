ae7e2cebfef85e236a7d1f53b931d99242994a15
lugo8 <austinslugo@gmail.com>
2024-08-25 15:08:49 -0400

    Wrote insert. Need to test

--- hash_list.cpp
+++ hash_list.cpp
@@ -1,12 +1,69 @@
 #include "hash_list.h"
 
-hash_list::hash_list() {}
+hash_list::hash_list() 
+{
+    size = 0;
+    head = nullptr;
+    iter_ptr = nullptr;
+}
 
 /**-----------------------------------------------------------------------------------
  * START Part 1
  *------------------------------------------------------------------------------------*/
 
-void hash_list::insert(int key, float value) {}
+void hash_list::insert(int key, float value) 
+{
+    node* iter_ptr = head;
+
+    //Check to see if key is in bounds of linked list or
+    //is adding beyond one bigger than current list
+    if(key < 0 || key > size)
+    {
+        return std::nullopt;
+    }
+
+    //If adding to end of list
+    if(key == size) 
+    {
+        //Empty list
+        if(head == nullptr)
+        {
+            //Initialize the list
+            node first = {.key = 0, .value = value, .next = nullptr};
+            head = &first;
+        }
+
+        else
+        {
+            //Find end of list
+            while(iter_ptr != nullptr && iter_ptr->next != nullptr)
+            {
+                iter_ptr = iter_ptr->next;
+            }
+
+            node last = {.key = size, .value = value, .next = nullptr};
+
+            iter_ptr->next = &last;
+
+        }
+
+        size++;
+        
+    }
+
+    //Replacing a current key/value pair
+    else
+    {
+        //Find the key
+        while(iter_ptr->key != key)
+        {
+            iter_ptr = iter_ptr->next;
+        }
+
+        iter_ptr->value = value;
+
+    }
+}
 
 std::optional<float> hash_list::get_value(int key) const { return std::nullopt; }
 
